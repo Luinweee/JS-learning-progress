40e65d77ea0fb2b3433685bd4d38d2cfc5da0b4b
Omer ERKAN <m.omererkan@outlook.com>
2023-12-20 23:04:52 +0300

    Queryselector

--- DOM Challenge Starting Files/index.js
+++ DOM Challenge Starting Files/index.js
@@ -1,1 +1,7 @@
-document.firstElementChild.lastElementChild.lastElementChild.lastElementChild.innerHTML = "Angela"
+document.querySelector(".list a").style.color = "red";
+document.querySelector("h1").style.color = "red";
+document.querySelector("button").style.backgroundColor = "yellow";
+document.querySelector("h1").classList.add("huge");
+document.querySelector("h1").innerHTML = "<em>GOOD BYE</em>";
+
+
